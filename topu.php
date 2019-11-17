7d2ea14b5482c3a31e8c5edfc47c059639830731
2016-2-60-065 <2016-2-60-065@cse.ewu>
2019-11-17 11:56:59 +0600

    topu created

---
+++ topu.php
@@ -0,0 +1,42 @@
+<?php 
+	session_start();
+	$db = mysqli_connect('localhost', 'root', '', 'crud');
+
+	// initialize variables
+	$name = "";
+	$address = "";
+	$id = 0;
+	$update = false;
+
+	if (isset($_POST['save'])) {
+		$name = $_POST['name'];
+		$address = $_POST['address'];
+
+		mysqli_query($db, "INSERT INTO info (name, address) VALUES ('$name', '$address')"); 
+		$_SESSION['message'] = "Address saved"; 
+		header('location: admin.php');
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD: CReate, Update, Delete PHP MySQL</title>
+</head>
+<body>
+    <?php echo "zakir"; ?>
+	<form method="post" action="server.php" >
+		<div class="input-group">
+			<label>Name</label>
+			<input type="text" name="name" value="">
+		</div>
+		<div class="input-group">
+			<label>Address</label>
+			<input type="text" name="address" value="">
+		</div>
+		<div class="input-group">
+			<button class="btn" type="submit" name="save" >Save</button>
+		</div>
+	</form>
+</body>
+</html>
